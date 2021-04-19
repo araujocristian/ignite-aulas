234a9e8cd79a09a6bddf3e47890e4840d01c56ec
Cristian Araujo <araujocristian@live.com>
2021-04-19 20:40:52 -0300

    feat: Estilização da listagem

--- 01-github-explorer/src/App.jsx
+++ 01-github-explorer/src/App.jsx
@@ -1,12 +1,6 @@
 import { RepositoryList } from "./components/RepositoryList";
-import { Counter } from "./components/Counter";
 import "./styles/global.scss";
 
 export function App() {
-  return (
-    <>
-      <RepositoryList />
-      <Counter />
-    </>
-  );
+  return <RepositoryList />;
 }
