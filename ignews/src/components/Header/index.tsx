5a67d63171f25419a9599f1089e8b74b7bbe2fd4
Cristian Araujo <araujocristian@live.com>
2021-05-22 21:31:44 -0300

    feat: Componente: Header

---
+++ ignews/src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import styles from "./styles.module.scss";
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <a className={styles.active} href="">
+            home
+          </a>
+          <a href="">posts</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
