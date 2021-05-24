5764caf34ef4cfc33a9e01888429edb284e64d7d
Cristian Araujo <araujocristian@live.com>
2021-05-24 19:58:01 -0300

    feat: Componente: SignInButton

--- ignews/src/components/Header/index.tsx
+++ ignews/src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
 export function Header() {
   return (
@@ -10,6 +11,8 @@ export function Header() {
           </a>
           <a href="">posts</a>
         </nav>
+
+        <SignInButton />
       </div>
     </header>
   );
